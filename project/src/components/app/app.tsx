55d6a76a36ee016ebc5d18237725d92de8b11731
vyatkinsen <vyatkinsen@bk.ru>
2023-05-21 21:48:27 +0300

    task 2.9 done

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main-page/main-page';
+
+type MainProp = {
+  title: string;
+  genre: string;
+  year: number;
+}
+
+function App({title, genre, year}: MainProp): JSX.Element {
+  return (
+    <Main
+      title = {title}
+      genre = {genre}
+      year = {year}
+    />
+  );
 }
 
 export default App;
